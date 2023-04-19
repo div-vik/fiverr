28fe54aece27f28e24e97f0c7aeaa3fcb84912ef
SidZhoop <siddharth@zhoop.in>
2023-04-19 22:31:37 +0530

    completed

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://fiverr-server-6o7v.onrender.com/api/",
   withCredentials: true,
 });
 
